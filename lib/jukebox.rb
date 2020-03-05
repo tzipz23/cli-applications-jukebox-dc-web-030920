0aa4602b7713f3f21e0a8b84e44a4ccc097bdbb0
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-05 02:08:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -4,7 +4,7 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts "- exit : exits this program"
+  puts "- exit : please enter a command:"
 end
 
 def list(songs)
