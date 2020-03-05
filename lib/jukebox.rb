b580b085d0a01aeac3f2feb295210925893ef721
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-05 02:06:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,5 @@
 def help
-  puts "-Please enter a command:"
+  
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
